c53ac3b95ef7a2dfb4aec1122de8cc5c08f2f6c9
ZCXL <1341772976@qq.com>
2015-12-20 02:48:55 +0000

    2015-12-9

--- app/src/main/java/com/zhuchao/mobilehealth/RegisterActivity.java
+++ app/src/main/java/com/zhuchao/mobilehealth/RegisterActivity.java
@@ -1,12 +1,12 @@
 package com.zhuchao.mobilehealth;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class RegisterActivity extends ActionBarActivity {
+public class RegisterActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
